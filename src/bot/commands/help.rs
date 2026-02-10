94a5352dcfdf05f4a68cff6677dcc777d09fb1cd
Gorciu <gorciuyt@gmail.com>
2026-02-10 11:30:55 +0100

    format help

--- src/bot/commands/help.rs
+++ src/bot/commands/help.rs
@@ -14,7 +14,18 @@ pub async fn help(
         .embed(serenity::CreateEmbed::new()
             .title("Witaj w ekonomii!")
             .field("Komendy", "Generalnie na chwilę obecną możesz używać prawie każdej komendy oprócz hazardu jak w każdym innym bocie ekonomicznym tj. `bal`, `withdraw`, `work`, `slut`, `crime`, `deposit`, `ping`, `pay`, `rob`, `topmoney`, `shop`, `buy`. Są też popularne aliasy, np. `deposit` -> `dep`. To dalej alpha, więc trochę niedopracowane, ale lepsze to niż nic.", false)
-            .field("Hazard", "- **automaty**: Generalnie używasz `slots` i możesz po tym podać kwotę jaką chcesz obstawić na automatach. Daje to bardzo duże zyski, ale jest mała szansa na wygraną...\n- **rzut monetą:** To jest useful w pierwszych fazach gry, ale potem zbytnio nie, bo jest zbyt OP. Używasz tego generalnie tak, że `coinflip` i potem albo h albo t, a następnie no to ile stawiasz.\n- **blackjack**: Absolutny klasyk gatunku. Używasz `blackjack` i potem dajesz liczbę. Wtedy zyskasz super hajs, jak umiesz w to grać.\n- **dice**: Co tu dużo mówić... Losujemy Ci liczbę od 1 do 100 no i masz ten... jak zdobędziesz więcej niż 55 to wygrywasz. Używasz `dice` i potem dajesz kwotę zakładu.\n- **crash**: Też fajna gra, generalnie inwestujesz w shady akcje i patrzysz jak twoje pieniądze rosną. Musisz uciec zanim się j*bną na łeb i na szyję.", false)
+            .field(
+                "Hazard", 
+                format!(
+                    "{}\n{}\n{}\n{}\n{}", 
+                    "- **automaty**: Generalnie używasz `slots` i możesz po tym podać kwotę jaką chcesz obstawić na automatach. Daje to bardzo duże zyski, ale jest mała szansa na wygraną...", 
+                    "- **rzut monetą:** To jest useful w pierwszych fazach gry, ale potem zbytnio nie, bo jest zbyt OP. Używasz tego generalnie tak, że `coinflip` i potem albo h albo t, a następnie no to ile stawiasz.",
+                    "- **blackjack**: Absolutny klasyk gatunku. Używasz `blackjack` i potem dajesz liczbę. Wtedy zyskasz super hajs, jak umiesz w to grać.", 
+                    "- **dice**: Co tu dużo mówić... Losujemy Ci liczbę od 1 do 100 no i masz ten... jak zdobędziesz więcej niż 55 to wygrywasz. Używasz `dice` i potem dajesz kwotę zakładu.", 
+                    "- **crash**: Też fajna gra, generalnie inwestujesz w shady akcje i patrzysz jak twoje pieniądze rosną. Musisz uciec zanim się j*bną na łeb i na szyję."
+                ),
+                false
+            )
             .field("Pomoc w tworzeniu", "Jeżeli chcesz pomóc w tworzeniu tego bota, no to możesz [zrobić pull request](<https://github.com/eclairbakery/Casino/pulls>) z jakąś funkcją, poprawką, czy czymkolwiek. Jakby coś, to tylko ekonomia. Główny bot, od prawie wszystkiego innego, jest [tutaj](<https://github.com/eclairbakery/EclairBOT>).", false)
         )
     ).await?;
